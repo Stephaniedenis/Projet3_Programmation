ee5f57f15e325d22b6f15ce7dab42f6d34beb0a9
Stephaniedenis <93406932+Stephaniedenis@users.noreply.github.com>
2022-04-27 15:50:11 -0400

    j'ai fait le schéma subscriber

---
+++ model/subscriber.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const subscriberSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required:true
+    }
+});
+subscriberSchema.methods.getInfo = function(){
+    return `Name: ${this.name} Email: ${this.email} Password: ${this.password}`
+};
+module.exports = mongoose.model("Subscriber", subscriberSchema);
